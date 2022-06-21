7d101cd6fab5f2777cbb4355722bfc9a89aba6f2
dskottsov <107855147+dskottsov@users.noreply.github.com>
2022-06-21 16:04:31 +0300

    test

--- Lab1.1/HelloWorld.py
+++ Lab1.1/HelloWorld.py
@@ -1,4 +1,15 @@
+import sys
 
 print ("Hello World!")
 print ("Hello World!")
+
+s="This is another text"
+print (sys.getsizeof(s))
+
+def test(x):
+    print(x)
+    return x*x
+
+print(test(50)+test(20))
+
 
