5f4cca89ea18d935cddc35f36954d9ea0ee9709c
dskottsov <107855147+dskottsov@users.noreply.github.com>
2022-06-23 18:32:03 +0300

    Lab1.4

--- Lab1.4/Lab1.4.py
+++ Lab1.4/Lab1.4.py
@@ -1,16 +1,38 @@
-class Subnet:
-    def __init__(self, n="0.0.0.0", p="/0"):
-        self.network = n
-        self.prefix = p
-    def __str__(self):
-        return self.network + self.prefix
-    def getnet(self):
-        return self.network
-    def getprefix (self):
-        return self.prefix
+import ipaddress
+import random
 
-n1 = Subnet("192.168.1.0","/24")
-print(n1.getnet(),n1.getprefix())
-print(n1)
+
+#a = random.randint(8, 24)
+#b = random.randint(0x0B000000, 0xDF000000)
+#net1 = ipaddress.IPv4Network((b,a), strict=False)
+
+#print (net1)
+
+class IPv4RandomNetwork(ipaddress.IPv4Network):
+    def __init__(self, a=1, b=1):
+        self.ip_addr=random.randint(0x0b000000, 0xdf000000)
+        self.ip_net=random.randint(8,24)
+        ipaddress.IPv4Network.__init__(self, (self.ip_addr, self.ip_net), strict=False)
+
+test1 = IPv4RandomNetwork()
+
+class Networks_list(IPv4RandomNetwork):
+    def ret_random_net(self):
+        for i in range(0, 50):
+            rn = IPv4RandomNetwork()
+            return rn
+
+test2=Networks_list()
+
+print(test2.ret_random_net())
+
+
+
+
+
+
+
+
+
 
 
